84baed82d49c5ef83d1b7e7c339e33b847fba795
MG <michal.gulczynski93@gmail.com>
2020-02-14 15:07:52 +0100

    Used unittest module added comments for task list app

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome(r"E:\Downloads\chromedriver.exe")
-browser.get('http://localhost:8000')
 
-assert "Django" in browser.title
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Chrome(r"E:\Downloads\chromedriver.exe")
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn("Listy", self.browser.title)
+        self.fail("Zakonczenie testu!")
+
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
+
+
