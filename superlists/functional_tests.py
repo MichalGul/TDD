4594f022bc6751674a6ef76e60e5794236a0089a
MG <michal.gulczynski93@gmail.com>
2020-03-01 13:19:39 +0100

    Kod HTML strony głównej jest teraz generowany na podstawie szablony

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -21,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Zwróciła uwagę, że tytuł strony i nagłówek zawierają słowo Listy.
         self.assertIn("Listy", self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').header_text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Listy', header_text)
 
         #Wpisywanie rzeczy do zrobienia
@@ -42,7 +42,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue (
-            any(row.text == '1: Kupić pawie pióra' for row in rows)
+            any(row.text == '1: Kupić pawie pióra' for row in rows),
+            "Nowy element nie znajduje się w tabeli"
         )
         # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
         # Edyta wpisała "Użyć pawich piór do zrobienia przynęty" (Edyta jest niezwykle skrupulatna).
