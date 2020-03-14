5bcde761d391f309199f932c6ca20a9e49383639
DESKTOP-NGBNHNQ\Michał <michal.gulczynski93@gmail.com>
2020-03-14 19:17:28 +0100

    Przekierowanie po żądaniu POST, wyswietlanie wszystkich elementów w szablonie

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from lists.models import Item
 # Create your views here.
 
 
+# def home_page(request):
+#     if request.method == 'POST':
+#         new_item_text = request.POST['item_text']
+#         Item.objects.create(text=new_item_text)
+#     else:
+#         new_item_text=""
+
+#     return render(request, 'home.html', {
+#         'new_item_text': new_item_text,
+#     }) #D jango automatycznie szuka katalogow templates szuka pliku html i tworzony jest http request
+
 def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    }) #D jango automatycznie szuka katalogow templates szuka pliku html i tworzony jest http request
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    items = Item.objects.all()
+    return render(request, 'home.html', {"items": items}) 
