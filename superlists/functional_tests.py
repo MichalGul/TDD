8c5d0848eaaff61a3f4990ed1fcec1d67ca8804e
DESKTOP-NGBNHNQ\Michał <michal.gulczynski93@gmail.com>
2020-03-14 16:05:33 +0100

    Refactor testu funkcjonalnego

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -11,6 +11,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
+
 
     def test_can_start_a_list_and_retrive_it_later(self):
         # Opis scenariusza testu funkcjonalnego
@@ -38,16 +44,8 @@ class NewVisitorTest(unittest.TestCase):
         # Po naciśnięciu klawisza Enter strona została uaktualniona i wyświetla
         # "1: Kupić pawie pióra" jako element listy rzeczy do zrobienia.
         inputbox.send_keys(Keys.ENTER)
-
+        self.check_for_row_in_list_table("1: Kupić pawie pióra")
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue (
-        #     any(row.text == '1: Kupić pawie pióra' for row in rows),
-        #     "Nowy element nie znajduje się w tabeli -- jego tekst to: {}".format(table.text)
-        # )
-        self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
-
         # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
         # Edyta wpisała "Użyć pawich piór do zrobienia przynęty" (Edyta jest niezwykle skrupulatna).
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -55,13 +53,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # Strona została ponownie uaktualniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
-        self.assertIn(
-            '2: Użyć pawich piór do zrobienia przynęty', [row.text for row in rows]
-            )
-
+        self.check_for_row_in_list_table("1: Kupić pawie pióra")
+        self.check_for_row_in_list_table("2: Użyć pawich piór do zrobienia przynęty")
 
         # Edyta była ciekawa, czy witryna zapamięta jej listę. Zwróciła uwagę na
         # wygenerowany dla niej unikatowy adres URL, obok którego znajduje się
