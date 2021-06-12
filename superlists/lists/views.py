b8d7edbe88057eae3be78f1ce02d943832b136b8
Michał <mwarioxin@gmail.com>
2021-06-12 20:59:26 +0200

    Adresy URL dla dwóch formularzy, przeniesiony kod w widokach i testach, nowy adres URL

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -16,12 +16,17 @@ from lists.models import Item
 #     }) #D jango automatycznie szuka katalogow templates szuka pliku html i tworzony jest http request
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {"items": items})
+
+
+def new_list(request):
+    print(dir(request))
+    if request.method == "POST":
+        Item.objects.create(text=request.POST['item_text'])
+
+    return redirect('/lists/the-only-list-in-the-world/')
