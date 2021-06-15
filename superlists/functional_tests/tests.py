71890074b4c8e22aefb36140144e8f7fc7123a8d
Michał <mwarioxin@gmail.com>
2021-06-15 20:52:53 +0200

    Podstawowy test funkcjonalny dla układu i stylów

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -107,6 +107,25 @@ class NewVisitorTest(LiveServerTestCase):
 
 
 
+    def test_layout_and_styling(self):
+        # Edyta przeszla na strone glowna
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
+        #Zauwazyla elegancko wysrodkowane pole tekstowe
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            507,
+            delta=5
+        )
+
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
 
 
